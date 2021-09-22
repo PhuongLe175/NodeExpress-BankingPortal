82552d0550560275985a1f5a2a7b1c7d6c2c8c0c
Lê Thanh Phương <lethanhphuong17051989@gmail.com>
2021-09-22 09:54:59 +0700

    Completed Module

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const path = require('path');
 const express = require('express');
 const app = new express();
 
-const{accounts, users, writeJSON} = require('./data');
+const {accounts, users} = require('./data.js');
+const {accountRoutes} = require('./routes/accounts.js');
+const {servicesRoutes} = require('./routes/services.js');
+
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -13,30 +16,13 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({extended: true}));
 
 
-app.get('/', (req, res) => res.render('index', {title:"Account Summary", accounts: accounts}));
+app.get('/', (req, res) => res.render('index', {title: "Account Summary", accounts: accounts}));
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
-app.get('/savings', (req, res)=> res.render('account',{account: accounts.savings}));
-app.get('/checking', (req, res)=> res.render('account',{account: accounts.checking}));
-app.get('/credit', (req, res)=> res.render('account',{account: accounts.credit}));
+app.get('/profile', (req, res) => res.render('profile', {user: users[0]}));
 
-app.get('/transfer', (req, res) => res.render('transfer'));
-
-app.post('/transfer', (req, res) => {
-   accounts[req.body.from].balance -= req.body.amount;
-   accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-   writeJSON();
-   res.render('transfer', {message: 'Transfer Completed'});
+app.listen(3000, () => {
+    console.log('PS Project Running on port 3000!')
 });
-
-app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
-app.post('/payment', (req, res) => {
-   accounts.credit.balance -= req.body.amount;
-   accounts.credit.available += parseInt(req.body.amount);
-   writeJSON();
-   res.render('payment', {message: 'Payemnt Successful', account: accounts.credit})
-});
-
-
-app.get('/profile', (req, res) => res.render('profile', {user:users[0]}));
-app.listen(3000,() => {console.log('PS Project Running on port 3000!')});
 
